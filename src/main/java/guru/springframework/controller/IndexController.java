f57690ac309d16780e7dee1d1f0d65400fd7ca51
Igor.Kiv <igor.kiv@gmail.com>
2021-03-25 10:01:28 +0600

    165. Assignment - Complete Refactoring with Project Lombok

--- src/main/java/guru/springframework/controller/IndexController.java
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -5,12 +5,14 @@ import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
 import guru.springframework.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
     private final CategoryRepository categoryRepository;
@@ -30,6 +32,8 @@ public class IndexController {
         Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
         Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
 
+        log.debug("Getting Index Page");
+
         System.out.println("Cat ID is: " + categoryOptional.get().getId());
         System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
 
@@ -38,6 +42,9 @@ public class IndexController {
 
     @GetMapping("/recipes")
     String listOfRecipes(Model model){
+
+        log.debug("Getting List of Recipes Page");
+
         model.addAttribute("recipes",recipeService.getRecipes());
         return "recipes";
     }
